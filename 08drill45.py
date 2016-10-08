00b9b49fd7f3e55062b1873d0026255d3a2973e8
Tyler C <tyler.corum@gmail.com>
2016-10-07 18:06:41 -0700

    removed second function, changed var names, changed while loop to for loop

--- 08drill45.py
+++ 08drill45.py
@@ -24,41 +24,30 @@
 
 # 2. Creating two lists, want to get to one, comma separated
 listToSort1 = [67, 45, 2, 13, 1, 998]               #
-lengthLTS1 = len(listToSort1)                       # 6 items
+# lengthLTS1 = len(listToSort1)                       # 6 items
 listToSort2 = [89, 23, 33, 45, 10, 12, 45, 45, 45]  #
-lengthLTS2 = len(listToSort2)                       # 9 items
+# lengthLTS2 = len(listToSort2)                       # 9 items
 
 
 # 3. functions 1 and 2 for the separate lists
-def tylerSort1(listToSort1,lengthLTS1):
-    while lengthLTS1 > 1:  #this means it is only checking itself to it's right-item buddy, and if it's 1 it would check against self, so break
-        for i in range(0,lengthLTS1-1):
-            if listToSort1[i] >= listToSort1[i+1]:
-                temp = listToSort1[i]
-                listToSort1[i] = listToSort1[i+1]
-                listToSort1[i+1] = temp
-                if lengthLTS1 == 2:  #once only a pair left, break so not get stuck in loop
-                    break
-            else:
-                lengthLTS1 = lengthLTS1-1
+def sortList_Asc(listItem):
+    lengthOfList = len(listItem)
 
-def tylerSort2(listToSort2,lengthLTS2):
-    while lengthLTS2 > 1:  #this means it is only checking itself to it's right-item buddy, and if it's 1 it would check against self, so break
-        for i in range(0,lengthLTS2-1):
-            if listToSort2[i] >= listToSort2[i+1]:
-                temp = listToSort2[i]
-                listToSort2[i] = listToSort2[i+1]
-                listToSort2[i+1] = temp
-                if lengthLTS2 == 2:  #once only a pair left, break so not get stuck in loop
+    for eachPass in range(lengthOfList-1):
+        for i in range(0,lengthOfList-1):
+            if listItem[i] >= listItem[i+1]:
+                temp = listItem[i]
+                listItem[i] = listItem[i+1]
+                listItem[i+1] = temp
+                if lengthOfList == 2:  #once only a pair left, break so not get stuck in loop
                     break
             else:
-                lengthLTS2 = lengthLTS2-1
+                lengthOfList = lengthOfList-1
 
 
 # 4. processing the output
-tylerSort1(listToSort1,lengthLTS1)
+sortList_Asc(listToSort1)
 print(listToSort1)
 print('\n')
-tylerSort2(listToSort2,lengthLTS2)
+sortList_Asc(listToSort2)
 print(listToSort2)
-
