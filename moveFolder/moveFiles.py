bef712d5c5043682d2e0e14d659463d6ddc587f8
Tyler C <tyler.corum@gmail.com>
2016-10-13 18:08:04 -0700

    added program to move files from Folder A to Folder B and print directory on screen

--- moveFolder/moveFiles.py
+++ moveFolder/moveFiles.py
@@ -1,16 +1,39 @@
+# Python 3.5.2 IDLE x86/32-bit
+# Author: Tyler Corum, 10/11/2016
+# Purpose: Move files from Folder A to Folder B and print the directory to the screen.
+#
+# Tested On: Windows 7 x64
+
+
+# ____________________Table Of Contents____________________
+#
+# 1. Import
+# 2. Processing
+#
+# 999. TESTING
+#
+# /$\ # urgent and important
+# /!\ # important
+# /#\ # important but not ugent
+# /?\ # error?/unknown?
+
+
+# 1. Import
+# ---------
 import shutil
 import os
-import pdb
+# import pdb
 
 
-pdb.set_trace()
+# 2. Processing
+# -------------
+# pdb.set_trace()
 
-#shutil.move(src, dst, copy_function=copy2)
 source = 'C:\\Projects\\Python\\pythonDrills\\moveFolder\\Folder_A\\'
 destination = 'C:\\Projects\\Python\\pythonDrills\\moveFolder\\Folder_B\\'
 
 files = os.listdir(source)
 
-for f in files:
-	print(f)
-# shutil.move(source, destination)
+for file in files:
+	print('{}\tmoved to {}\{}'.format(file,destination,file))
+	shutil.move(source + file, destination)
