9f0174ca7c160d221be85238201e071cbd75c0d1
Tyler C <tyler.corum@gmail.com>
2016-10-14 11:11:04 -0700

    added some formatting for display

--- moveFolder2/moveFiles.py
+++ moveFolder2/moveFiles.py
@@ -1,13 +1,13 @@
 # Python 3.5.2 IDLE x86/32-bit
 # Author: Tyler Corum, 10/14/2016
-# Purpose: Move files from last 24 hours into a new folder called "destination"
+# Purpose: Copy .txt files from last 24 hours into a new folder called "destination"
 #
 # Tested On: Windows 7 x64
 
 
 # ____________________Table Of Contents____________________
 #
-# 1. Import
+# 1. Import and functions
 # 2. Processing
 #
 # 999. TESTING
@@ -18,15 +18,14 @@
 # /?\ # error?/unknown?
 
 
-# 1. Import
+# 1. Import and functions
 # ---------
 import shutil
 import os
 import time
 import math
 import datetime
-# import pdb
-
+import pdb
 
 def last_24(unixUTC):
 	source = 'C:\\Projects\\Python\\pythonDrills\\moveFolder2\\Folder_A\\'
@@ -35,16 +34,25 @@ def last_24(unixUTC):
 	files = os.listdir(source)
 	
 	for file in files:
+		strLength = len(file)
+		
+		# pdb.set_trace()
+		fileNameDisplay = file
+		if strLength > 12:
+			# truncate = (len(file)-14)
+			fileNameDisplay = file[-12:]
 		if file.endswith('.txt'):
 			timestamp = os.lstat(source + file).st_mtime
 			if timestamp >= unixUTC - 86400:
-				print('{}\'s timestamp: {}.  Difference between timestamp and NOW: {}'.format(file,timestamp, unixUTC - timestamp))
-				shutil.copy(source+file, destination)
-				shutil.copystat(source + file, destination + file)
+				# print('{}\'s last-modified timestamp: {}.  Difference between timestamp and NOW: {}'.format(file,timestamp, unixUTC - timestamp))
+				shutil.copy(source+file, destination, follow_symlinks=False)
+				shutil.copystat(source + file, destination + file, follow_symlinks=False)
 				# copy success, copystat success as well
-				print('{} copied to:\t{}{}\ncopystat: {}\n'.format(file,destination,file,shutil.copystat(source + file, destination)))
-		# else:
-		# 	print('{}\tnot moved'.format(file))
+				print('{:.>14.12}\t copied to: {:.>35.32}{}'.format(fileNameDisplay,destination[-32:],file,))
+			elif timestamp < unixUTC - 86400:
+				print('{:.>14.12}\t not copied.  File older than 24 hours.'.format(fileNameDisplay))
+		else:
+			print('{:.>14.12}\t not copied.  File not *.txt format.'.format(fileNameDisplay))
 	return
 
 def time_now():
@@ -52,18 +60,16 @@ def time_now():
 	unixUTC = math.ceil(time.time())
 	date = str(datetime.datetime.fromtimestamp(unixUTC).strftime('%Y-%m-%d %H:%M:%S'))
 
-	print('unix timestamp right NOW: {}\nconverted into time: {}'.format(unixUTC, date))
-
+	print('unix timestamp right NOW: {}\nconverted into time: {}\n------------------------------------------------------\n'.format(unixUTC, date))
 	last_24(unixUTC)
 	return
 
-time_now()
 
 # 2. Processing
 # -------------
 # pdb.set_trace()
 
-
+time_now()
 
 
 
@@ -75,16 +81,3 @@ time_now()
 
 
 # # 999. TESTING
-
-
-# for file in files:
-# 	if file.endswith('.txt'):
-# 		timestamp = os.lstat().st_mtime
-# 		print('{}\tmoved to {}{}\n\ntimestamp: {}'.format(file,destination,file,timestamp))
-# 		shutil.copystat(source + file, destination)
-
-
-
-# t1 = os.lstat('test.txt').st_mtime
-
-# shutil.copystat
